64be7d087420a02882a6a4179372776119be8f35
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-18 22:41:59 +0530

    Create Multiple Comment in Python.py

---
+++ Day2/Multiple Comment in Python.py
@@ -0,0 +1,10 @@
+"""
+this is multiple linr comment
+you can comment in multiple line by just using  triple single
+or double imverted comma
+
+"""
+
+print("Hey this is day2 in python")
+
+#output: Hey this is day2 in python

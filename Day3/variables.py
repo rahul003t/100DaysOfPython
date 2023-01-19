7571013a2f0863911c9680002c01f3e461dcb62d
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-19 23:56:15 +0530

    Create variables.py

---
+++ Day3/variables.py
@@ -0,0 +1,29 @@
+#it is a container in programming laguages that hold data
+# like int float long char string as examples
+
+
+#Examples
+
+
+a=10
+b="Rahul"
+c=1.1
+d=None
+
+print(a)
+print(b)
+
+print("the type of a is :", type(a))
+print("the type of b is :",type(b))
+print("The type of c is :",type(c))
+
+"""
+output:
+
+10
+Rahul
+the type of a is : <class 'int'>
+the type of b is : <class 'str'>
+The type of c is : <class 'float'>
+
+"""

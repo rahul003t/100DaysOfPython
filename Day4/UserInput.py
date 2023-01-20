6c4964f80f841e53891990517d52098ba6bbd437
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-20 17:00:32 +0530

    Create UserInput.py

---
+++ Day4/UserInput.py
@@ -0,0 +1,13 @@
+a=input("Enter first Number : ")
+b=input("Enter second Number :")
+
+print(a+b)
+print(int(a)+int(b))
+
+"""
+Output:
+Enter First Number : 12
+Enter second Number : 100
+
+121000 # without casting pytho takes input as string
+112    # with explicitely casting int int datatype

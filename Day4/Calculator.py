9ce6890e845a5f87756f01da635799c3e83607de
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-20 16:42:03 +0530

    Create Calculator.py

---
+++ Day4/Calculator.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number :"))
+b=int(input("Enter a another number :"))
+
+print("Addition is :", a+b)
+print("Substraction is :",a-b)
+print("Multiplication is :",a*b)
+print("Modulo is :",a%b)
+
+"""
+Output:
+enter a number :5
+Enter a another number :6
+Addition is : 11
+Substraction is : -1
+Multiplication is : 30
+Modulo is : 5
+"""

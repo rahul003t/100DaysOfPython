b20f8eff09c342c00477707e94be3df986f9a523
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-21 23:54:31 +0530

    Create String.py

---
+++ Day5/String.py
@@ -0,0 +1,76 @@
+# Lets dicuss about String in PYthon
+name="Rahul"
+print(name)
+
+"""
+output:
+Rahul
+"""
+
+#we can declare a string by using single inverted comma or double inverted comma
+# If you use multiple line string print or print all the line as whatever user type then we use triple inverted comma ( """......""")
+
+st ="""my name is Rahul
+I am a 3rd year IT student
+I am a Web Developer"""
+print(st)
+
+"""
+Output:
+my name is Rahul
+I am a 3rd year IT student
+I am a Web Developer
+
+"""
+
+# string's Indexing start fromr 0
+#lets take an exaple
+
+name2="David"
+print(name2[0])
+print(name2[1])
+print(name2[2])
+print(name2[3])
+print(name2[5])
+
+"""
+Output:
+D
+a
+v
+i
+d
+
+"""
+
+#we can alse print the whole string by using for loop
+
+st2="Krishna"
+for character in st2:
+    print(characteer)
+  
+  
+  """
+  Output:
+  K
+  r
+  i
+  s
+  h
+  n
+  a
+  
+  """
+
+
+
+
+
+
+
+
+
+
+
+
+

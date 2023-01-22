116c2519ddad96d0a8df02d792d0210408953d4f
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-22 23:58:56 +0530

    Create StringSlicing.py

---
+++ Day6/StringSlicing.py
@@ -0,0 +1,46 @@
+
+name ="Rahul"
+print(name[0:5])
+
+"""
+Output:
+Rahul
+
+"""
+######################################
+#print the length of the string
+name="Rahul"
+print(len(name))
+"""
+Output:
+5
+
+"""
+
+#########################################
+fruit = "Mango"
+mangoLen=len(fruit)
+
+print(mangoLen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-3])  #negative slicing
+print(fruit[0:len(fruit)-3]) # this is done by default
+print(fruit[-3:-1]
+      
+
+"""
+Output:
+5
+Mang
+ang
+Mango
+Ma
+Ma
+ng
+
+"""
+
+#################################################
+
